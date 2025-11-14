b0c35dadf5e9ed96cda627d5a69ce83af5307c9a
Gaurav <gauravdhokane20@gmail.com>
2025-11-14 19:46:43 +0530

    Interpolation-in-Angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
 import { Component, signal } from '@angular/core';
 
-
 @Component({
   selector: 'app-root',
   imports: [],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
+//Interpolation
+/*
+  used to pass small data from TS file to html
+  we can perform js operations and functions using interpolation
+  Interpolation works only in templateUrl html file
+  we can not declare variable , can not increment or decrement variables in interpolation
+*/
 export class App {
   protected readonly title = signal('Angular-1st-Project');
+  name="Gaurav";
+  a=10;
+  b=20;
 }
