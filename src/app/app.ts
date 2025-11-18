66705fc42c085d80ca0320c7ba56e3f242febb35
Gaurav <gauravdhokane20@gmail.com>
2025-11-18 20:00:04 +0530

    Properties-and-datatypes

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,20 +8,25 @@ import { Login } from './login/login';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-//Click Event on button
+//Data types in Angular
 /*
-  Click event on btn , for that we have made 2 function
-  to make click event on that function, we need to pass that function
-  as full fun name with parameters
+  Properties and data types are 2 diff things
+  Properties are the variables which we define in class where data type is the type of that properties
 */
 export class App {
   protected readonly title = signal('Angular-1st-Project');
 
-  handleClickEvent(){
-    console.log("Button Clicked!!")
-    this.handleClickEvent2();//We have to use this is want to call function from same class
+  name="Gaurav";//by default angular decides data type of name as string
+  num=10;//data type of num is number;
+  data:string|boolean = "tata";//we can assign both type of values string as well as boolean to data
+  data2:any = 20;// we can assign any type of data to data2
+
+  updatevars(){
+    //this.name=20;<- this will give error
+    this.data2 = "tata";//it is accepting this
   }
-  handleClickEvent2(){
-    console.log("2nd function called");
+
+  sum(a:number,b:number){//need to pass same type of data to call this function
+    console.log(a+b);
   }
 }
