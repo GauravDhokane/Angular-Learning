df6ae07b5fd42bc371b9d4cc69a3c31186741370
Gaurav <gauravdhokane20@gmail.com>
2025-11-23 11:49:00 +0530

    For-loop

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,21 +8,22 @@ import { Login } from './login/login';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-//Switch in angular
+//ForLoop in angular
 /*
   widely used in many conditions and have default statement also
   but we need to pass variable for which we are checking the values
 */
 export class App {
   protected readonly title = signal('Angular-1st-Project');
-  backcolor='red';
+  users=["gaurav","dhokane","bunty","tata"];
 
-  handlecolor(val:string){
-    this.backcolor=val;
+  students=[
+    {name:"garry",age:29,email:"@garry.com"},
+    {name:"suru",age:21,email:"@suru.com"},
+    {name:"harry",age:24,email:"@harry.com"}
+  ]
+
+  GetName(val:string){
+    console.log(val);
   }
-
-  handlecolorinput(event:Event){
-    this.backcolor=(event.target as HTMLInputElement).value;
-  }
-
 }
