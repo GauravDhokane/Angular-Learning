1c8e6769613b8b409e11dcdeec632636e9a8c191
Gaurav <gauravdhokane20@gmail.com>
2025-11-25 22:00:53 +0530

    Effects-Usecase

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,25 +8,35 @@ import { Login } from './login/login';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-//Signals in angular
+//Effects in angular
 /*
-  Computed Signals are read only signals, but updates when it depends on other signals
+  Effects are declared in Constructor and it tell us when there is a change in value of signal
+  it automatically called when there is change in value of signal
+  We use the effect for letting us know that a signal has been updated! We generally write it inside
+  the constructor so that it gets initialized when the component is created
+  (the constructor runs only once), ensuring it starts tracking changes immediately.
+  However, once initialized, the effect itself automatically re-executes whenever the signal
+  gets updated, completely independent of the constructor—meaning it will keep
+  reacting to changes regardless of where or how the signal updates!)
 */
 export class App {
   protected readonly title = signal('Angular-1st-Project');
 
-  x=signal(10);
-  y=signal(20);
-  z=computed(()=>this.x()+this.y());
-
-  updatesignal(){
-    console.log(this.z());
-    //this.z.set(200);->this will give error
-    this.x.set(100);
-    console.log(this.z());//here it updates value of z as value of x is updating
-    // but if we use simple variable then value of z will nnot updates because addition has been done before
-    //value change of x
-
+  username=signal("Anil");
+  display=false;
+  count =signal(0);
+  constructor(){
+    effect(()=>{
+      if(this.count()==2){
+        this.display=true;
+      }
+      else{
+        this.display=false;
+      }
+    })
   }
 
+  updateCount(){
+    this.count.set(this.count()+1);
+  }
 }
