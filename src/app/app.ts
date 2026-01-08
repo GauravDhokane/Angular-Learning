31e788388d71246ecd0e355e089430a7634e3ec8
Gaurav <gauravdhokane20@gmail.com>
2026-01-08 20:34:23 +0530

    TO-DO-List

--- src/app/app.ts
+++ src/app/app.ts
@@ -11,11 +11,20 @@ import { FormsModule } from '@angular/forms';
 })
 //Two way binding
 /*
-  two way binding is used for data binding between HTML and TS file
-  two ways -> using ngmodule
-           -> using function
-  [(ngModel)] this is a import function that is used to make this binding
+
 */
 export class App {
-  name="";
+  task="";
+  tasklist:{id:number,task:string}[]=[];
+
+  addTask(){
+    this.tasklist.push({id:this.tasklist.length+1,task:this.task});
+    this.task="";
+    console.log(this.tasklist);
+  }
+  deletetask(index:number){
+    this.tasklist.splice(index-1, 1);
+    //splice() function taking 1st parameter as
+    //index and 2nd parameter as no of elements to remove starting from that index
+  }
 }
