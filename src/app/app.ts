35a25d8237508f633087ff08a98a7e2c8fd59304
Gaurav <gauravdhokane20@gmail.com>
2026-01-08 20:03:03 +0530

    Two-way-binding

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,21 @@
 import { Component, computed, effect, signal, WritableSignal } from '@angular/core';
 import { Login } from './login/login';
+import { FormsModule } from '@angular/forms';
 
 //below is known as Decorator ->@Component{}
 @Component({
   selector: 'app-root',
-  imports: [Login],
+  imports: [Login,FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-//Contextual variable in for loop in angular
+//Two way binding
 /*
-
+  two way binding is used for data binding between HTML and TS file
+  two ways -> using ngmodule
+           -> using function
+  [(ngModel)] this is a import function that is used to make this binding
 */
 export class App {
-  protected readonly title = signal('Angular-1st-Project');
-  users=["gaurav","tata","bata","vinay","tony","sony","moni"];
+  name="";
 }
