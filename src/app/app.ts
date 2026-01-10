c525714eeade37eb2283a8ab965dbb659e3e0d98
Gaurav <gauravdhokane20@gmail.com>
2026-01-10 11:50:11 +0530

    ngFor directives

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,24 +1,34 @@
 import { Component, computed, effect, signal, WritableSignal } from '@angular/core';
-import { Login } from './login/login';
-import { FormsModule } from '@angular/forms';
+import { NgFor } from '@angular/common';
 
 //below is known as Decorator ->@Component{}
 @Component({
   selector: 'app-root',
-  imports: [Login,FormsModule],
+  imports: [NgFor],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-//Directives In Angular
+//ngfor Directives In Angular
 /*
- Directives is a class which adds additional behaviour to element in application
- provide more power to dom element
- if-else condition
- ngclass,ngfor,ngif
- component directives
- structural directives
- attribute directived
+ we can loop in our template file using this (html file)
 */
 export class App {
-
+  students=["anil","peter","gaurav","tata","bata"];
+  studentdata=[
+    {
+      name:"garry",
+      age:20,
+      city:"Pune"
+    },
+    {
+      name:"harry",
+      age:21,
+      city:"Mumbai"
+    },
+    {
+      name:"toty",
+      age:23,
+      city:"Nagar"
+    }
+  ];
 }
