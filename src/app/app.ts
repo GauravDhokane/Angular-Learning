cb5aeb6c95c09a46f75c18b119eb5db451284bb9
Gaurav <gauravdhokane20@gmail.com>
2026-01-11 20:34:21 +0530

    ngSwitch directives

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,22 +1,22 @@
 import { Component, computed, effect, signal, WritableSignal } from '@angular/core';
-import { NgFor, NgIf } from '@angular/common';
+import { NgSwitch, NgSwitchCase } from '@angular/common';
 
 //below is known as Decorator ->@Component{}
 @Component({
   selector: 'app-root',
-  imports: [NgFor,NgIf],
+  imports: [NgSwitch,NgSwitchCase],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-//ngIf Directives In Angular
+//ngSwitch Directives In Angular
 /*
  we can apply if else using ngif
 */
 export class App {
-  show=true;
-  login=false;
-  val=0;
-  Incre(){
-    this.val++;
+  color="red";
+
+  Changecolor(reqColor:string)
+  {
+    this.color=reqColor;
   }
 }
