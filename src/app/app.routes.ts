8d0e7f1edeb45b9c248ce5ab78b14ae94435706c
Gaurav <gauravdhokane20@gmail.com>
2026-01-17 10:48:30 +0530

    Cleaning branch

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,7 @@
 import { Routes } from '@angular/router';
-import { About } from './about/about';
-import { Login } from './login/login';
-import { Contact } from './contact/contact';
-import { Home } from './home/home';
-import { PageNotFound } from './page-not-found/page-not-found';
-import { Profile } from './profile/profile';
-import { User } from './user/user';
+
 
 //this is the file where we have to enter all the components where we want to
 //route the angular page and also need to give path and component and need to import that
 
-export const routes: Routes = [
-  {path:'about',component:About},
-  {path:'login',component:Login},
-  {path:'contact',component:Contact},
-  {path:'home',component:Home},
-  {path:'user/:id/:name',component:User},
-  {path:'profile',component:Profile},// here we are passing id in url and based on that we will change UI
-  {path:'**',component:PageNotFound}
-];
+export const routes: Routes = [];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,22 +1,18 @@
 import { Component, computed, effect, signal, WritableSignal } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { Header } from './header/header';
+
 
 
 //below is known as Decorator ->@Component{}
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Header],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-// Forms in Angular
+// Basic reactive Forms in Angular
 /*
-  2 types of forms
-  reactive and template driven forms-> check official website
-  Simple type of forms - template driven
-  complex type of forms and validations then reactive forms
-
+  when we have to deal with complex forms then we need to check it
 */
 export class App {
 
