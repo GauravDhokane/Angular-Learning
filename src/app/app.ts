df7f5f1539c0358513cc4897f0f0ef2fdcaada3e
Gaurav <gauravdhokane20@gmail.com>
2026-01-17 11:13:48 +0530

    Basic Reactive Forms

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,5 @@
 import { Component, computed, effect, signal, WritableSignal } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 
@@ -6,14 +7,25 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 //below is known as Decorator ->@Component{}
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 // Basic reactive Forms in Angular
 /*
   when we have to deal with complex forms then we need to check it
+  what is formcontrol and reactiveforms we need to check that
+
 */
 export class App {
-
+  name=new FormControl();//we can give default value also here in the brackets with single inverted quoma
+  password = new FormControl();
+  displayValue(){
+    console.log(this.name.value);
+    console.log(this.password.value);
+  }
+  setValues(){
+    this.name.setValue('Gaurav');
+    this.password.setValue('32416272');
+  }
 }
