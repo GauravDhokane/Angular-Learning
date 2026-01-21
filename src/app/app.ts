3e1ad2a95c08eeaa6bcd3d740a5d1fe89c0d87a7
Gaurav <gauravdhokane20@gmail.com>
2026-01-21 20:19:25 +0530

    form-validation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,28 +1,37 @@
-import { Component, computed, effect, signal, WritableSignal } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { RouterLink, RouterOutlet } from '@angular/router';
-
+import { NgIf } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 
 //below is known as Decorator ->@Component{}
 @Component({
   selector: 'app-root',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule,NgIf],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-// Reactive form grouping
+// Reactive form Vaidations
 /*
-  FormGroup read about this
+  Validations
 
 */
 export class App {
 
   Profilegrp = new FormGroup({
-     name:new FormControl('Gaurav'),//we can give default value also here in the brackets with single inverted quoma
-     password:new FormControl('tata@123'),//this are the default values
-     email:new FormControl('garry@zoho.com')
+     name:new FormControl('',[Validators.required]),//we can give default value also here in the brackets with single inverted quoma
+     password:new FormControl('',[Validators.required,Validators.minLength(5)]),//this are the default values
+     email:new FormControl('',[Validators.required,Validators.maxLength(20),Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')]),
   })
+  //getter method of all props
+  get name(){
+    return this.Profilegrp.get('name');
+  }
+  get password(){
+    return this.Profilegrp.get('password');
+  }
+  get email(){
+    return this.Profilegrp.get('email');
+  }
   submit(){
     console.log(this.Profilegrp.value);
   }
