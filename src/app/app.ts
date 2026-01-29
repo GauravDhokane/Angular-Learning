4000c410b09665dbc4a0f5716120076e9f9e51f2
Gaurav <gauravdhokane20@gmail.com>
2026-01-29 20:23:32 +0530

    Template-driven-validation

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,7 +6,7 @@ import { FormControl, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Valid
 //below is known as Decorator ->@Component{}
 @Component({
   selector: 'app-root',
-  imports: [ReactiveFormsModule,FormsModule],
+  imports: [ReactiveFormsModule,FormsModule,NgIf],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
@@ -14,6 +14,7 @@ import { FormControl, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Valid
 /*
   the froms which is completly controlled from HTML file only
   this is widely used for basic forms only not complexed
+  //validations in template driven forms
 
 
 */
