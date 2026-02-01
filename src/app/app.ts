7654f34ab77011e2b3f31f2bcda82b3551bbe060
Gaurav <gauravdhokane20@gmail.com>
2026-02-01 11:15:43 +0530

    Custom pipes

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,13 @@ import { CommonModule, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
 import { User } from './user/user';
+import { CurrencyConverterPipe } from './pipe/currency-converter-pipe';
 
 
 //below is known as Decorator ->@Component{}
 @Component({
   selector: 'app-root',
-  imports: [User,CommonModule],
+  imports: [User,CommonModule,CurrencyConverterPipe],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
@@ -18,6 +19,9 @@ import { User } from './user/user';
 
   we have used multiple pipes and also 2 pipes for 1 thing 
   read about this imp
+
+  custom pipes in angular
+  read this we have created an currency converter using this 
 
 */
 export class App {
